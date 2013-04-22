77f359cc5e3e714e8f529156092696ae8a1b9760
Danielito <dseveso@hotmail.com>
2013-04-22 09:03:28 -0500

    New version for PS3
    Included new version for PS3 and small script to test queries to the
    thermostat

---
+++ TestTstatQuery.ps1
@@ -0,0 +1,14 @@
+﻿$s = $null
+#$url="http://192.168.1.76/tstat/"
+$url="http://192.168.1.76/sys/"
+$client = new-object net.webclient
+$s = $client.DownloadString($url)
+$cThermOut = $s | ConvertFrom-Json
+$s
+$cThermOut
+
+write-host "passed the connection statement"    
+#$hash = $s -replace '{|}|"' -replace ",","`n" -replace ":","=" | ConvertFrom-StringData
+
+#"Livingroom insideTemp = " + $hash["temp"]            
+#$insideTemp = $hash["temp"]  } 
